b22cdf516dbbdb62dd567a393add9a5a34f0a864
brittmouse <bstancoff@proton.me>
2024-02-04 11:53:33 -0500

    Very basic `throw_dice` using a vec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", throw_dice(6));
+}
+
+fn throw_dice(n: i32) -> Vec<i32> {
+    let mut dice_out: Vec<i32> = Vec::new();
+    for _ in 0..n {
+        dice_out.push(rand::thread_rng().gen_range(1..=6));
+    }
+    dice_out
 }
