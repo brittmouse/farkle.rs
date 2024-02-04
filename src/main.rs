231c21369237809d2fa6ef60a206639c33659768
brittmouse <bstancoff@proton.me>
2024-02-04 12:43:43 -0500

    Very minor vec rewrite
    I originally played with using a HashMap to store values.
    However it was being a pain so I've moved back to the Vec for now.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use rand::Rng;
+// use std::{collections::HashMap, io};
 
 fn main() {
+    // let mut points: u32 = 0;
     println!("{:?}", throw_dice(6));
 }
 
-fn throw_dice(n: i32) -> Vec<i32> {
-    let mut dice_out: Vec<i32> = Vec::new();
-    for _ in 0..n {
-        dice_out.push(rand::thread_rng().gen_range(1..=6));
-    }
+fn throw_dice(n: u32) -> Vec<u32> {
+    let mut dice_out: Vec<u32> = Vec::new();
+    (0..n).for_each(|_| dice_out.push(rand::thread_rng().gen_range(1..=6)));
     dice_out
 }
